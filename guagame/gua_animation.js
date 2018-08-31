e7acafa51a023c5fb467775e0f8e96db98f8a227
stone_liu <stone_liu@kingdee.com>
2018-08-31 18:50:23 +0800

    add

--- guagame/gua_animation.js
+++ guagame/gua_animation.js
@@ -20,38 +20,70 @@ class GuaAnimation {
 
         this.x = 100
         this.y = 100
-		this.texture = this.animations.idle[0]
-        this.w = this.texture.width / 2
-        this.h = this.texture.height / 2
-        log(1111, this.texture.width)
-        log(1111, this.texture.height)
-		this.framesIndex = 0
-		this.speed = 3
-		this.framesCount = this.speed
+		this.texture = this.frames()[0]
+        // log(1111, this.texture.width)
+        // log(1111, this.texture.height)
+		this.framesIndex = 0        	// 当前图片下标
+		this.speed = 3 			    	// 默认速度
+		this.flipX = false
+		this.framesCount = this.speed  
 	}
+
+	frames() {
+		return this.animations[this.animationName]
+	}
+
 	update() {
 		this.framesCount--
 		//  设置动画的定格图片
 		if (this.framesCount === 0) {
 			this.framesCount = this.speed
-			this.framesIndex = (this.framesIndex + 1) % this.animations[this.animationName].length
-			this.texture = this.animations[this.animationName][this.framesIndex]
-	        this.w = this.texture.width / 2
-	        this.h = this.texture.height / 2
+			this.framesIndex = (this.framesIndex + 1) % this.frames().length
+			this.texture = this.frames()[this.framesIndex]
+	        this.w = this.texture.width / 5
+	        this.h = this.texture.height / 5
 		}
 	}
+	
+	flipImage(image, ctx, flipH, flipV) {
+	    var scaleH = flipH ? -1 : 1, // Set horizontal scale to -1 if flip horizontal
+	        scaleV = flipV ? -1 : 1, // Set verical scale to -1 if flip vertical
+	        posX = flipH ? width * -1 : 0, // Set x position to -100% if flip horizontal 
+	        posY = flipV ? height * -1 : 0; // Set y position to -100% if flip vertical
+	    
+	    ctx.save(); // Save the current state
+	    ctx.scale(-1, scaleV); // Set scale to flip the image
+	    ctx.drawImage(image); // draw the image
+	    ctx.restore(); // Restore the last saved state
+	}
+
 	draw() {
+		if (this.flipX) {
+			this.flipImage(this, this.game.context)
+		}
 		this.game.drawImage(this)
 	}
+
+	changeAnimation(name, speed) {
+		this.speed = speed || this.speed
+		this.animationName = name
+	}
+
 	move(speed, keyStatus) {
 		this.x += speed
-		if (keyStatus == 'down') {
-			this.speed = 1
-			this.animationName = 'run'
-		} else {
-			this.speed = 3
-			this.animationName = 'idle'
+		// 代码优化
+		// if (keyStatus == 'down') {
+		// 	this.speed = 1
+		// 	this.changeAnimation('run')
+		// } else {
+		// 	this.speed = 3
+		// 	this.changeAnimation('idle')
+		// }
+		let animationNames = {
+			down: ['run', 1],
+			up: ['idle', 3],
 		}
-		log('keyStatus', keyStatus)
+		this.changeAnimation(...animationNames[keyStatus])
+		//  优化end
 	}	
 }
